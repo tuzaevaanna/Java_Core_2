47f5178b8a72defa2724db054fd3ee11a3c080df
Tuzaeva Anna <tuzaeva.anna@mail.ru>
2021-02-08 13:50:16 +0300

    ДЗ_3

---
+++ src/Lesson_3/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson_3;
+
+    abstract class Fruit {
+        private float weight;
+
+        Fruit(float weight) {
+            this.weight = weight;
+        }
+
+        public float getWeight() {
+            return weight;
+        }
+    }
